8803383b9a6b4b2d450603453a8291612126020e
Kieran <kieran@harkin.me>
2018-11-16 21:22:38 +0800

    init

---
+++ rust_wipe/ItemDef.cs
@@ -0,0 +1,281 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+
+namespace rust_wipe
+{
+
+    public class Phrase
+    {
+        public string token { get; set; }
+        public string english { get; set; }
+    }
+
+    public class IconSprite
+    {
+        public int instanceID { get; set; }
+    }
+
+    public class SoundDefinition
+    {
+        public int instanceID { get; set; }
+    }
+
+    public class FoundCondition
+    {
+        public double fractionMin { get; set; }
+        public double fractionMax { get; set; }
+    }
+
+    public class Condition
+    {
+        public bool enabled { get; set; }
+        public double max { get; set; }
+        public bool repairable { get; set; }
+        public bool maintainMaxCondition { get; set; }
+        public FoundCondition foundCondition { get; set; }
+    }
+
+    public class SteamItem
+    {
+        public int instanceID { get; set; }
+    }
+
+    public class Parent
+    {
+        public int instanceID { get; set; }
+    }
+
+    public class Panel
+    {
+        public int instanceID { get; set; }
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ItemCategory
+    {
+        Weapon,
+        Construction,
+        Items,
+        Resources,
+        Attire,
+        Tool,
+        Medical,
+        Food,
+        Ammunition,
+        Traps,
+        Misc,
+        All,
+        Common,
+        Component,
+        Search
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ItemSelectionPanel
+    {
+        None,
+        Vessel,
+        Modifications,
+        GunInformation
+    }
+
+    [Flags]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ItemSlot
+    {
+        None = 1,
+        Barrel = 2,
+        Silencer = 4,
+        Scope = 8,
+        UnderBarrel = 0x10
+    }
+
+    [Flags]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum ContentsType
+    {
+        Generic = 1,
+        Liquid = 2
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum AmountType
+    {
+        Count,
+        Millilitre,
+        Feet,
+        Genetics,
+        OxygenSeconds
+    }
+
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum Rarity
+    {
+        None,
+        Common,
+        Uncommon,
+        Rare,
+        VeryRare
+    }
+
+    [Flags]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum Flag
+    {
+        NoDropping = 1,
+        NotStraightToBelt = 2
+    }
+
+    [Flags]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum TraitFlag
+    {
+        None = 0,
+        Alive = 1,
+        Animal = 2,
+        Human = 4,
+        Interesting = 8,
+        Food = 0x10,
+        Meat = 0x20,
+        Water = 0x20
+    }
+
+    public class ItemDefinition
+    {
+        public int itemid { get; set; }
+        public string shortname { get; set; }
+        public Phrase displayName { get; set; }
+        public Phrase displayDescription { get; set; }
+        public IconSprite iconSprite { get; set; }
+        public ItemCategory category { get; set; }
+        public ItemSelectionPanel selectionPanel { get; set; }
+        public int maxDraggable { get; set; }
+        public ContentsType itemType { get; set; }
+        public AmountType amountType { get; set; }
+        public int occupySlots { get; set; }
+        public int stackable { get; set; }
+        public bool quickDespawn { get; set; }
+        public Rarity rarity { get; set; }
+        public bool spawnAsBlueprint { get; set; }
+        public SoundDefinition inventorySelectSound { get; set; }
+        public SoundDefinition inventoryGrabSound { get; set; }
+        public SoundDefinition inventoryDropSound { get; set; }
+        public SoundDefinition physImpactSoundDef { get; set; }
+        public Condition condition { get; set; }
+        public bool hidden { get; set; }
+        public Flag flags { get; set; }
+        public SteamItem steamItem { get; set; }
+        public Parent Parent { get; set; }
+        public ObjectRef worldModelPrefab { get; set; }
+        public TraitFlag Traits { get; set; }
+        public Panel panel { get; set; }
+
+        public List<ItemMod> Mods { get; set; } = new List<ItemMod>();
+    }
+
+    public class ItemAmount
+    {
+        public ItemDefinition itemDef { get; set; }
+
+        public float amount { get; set; }
+
+        public int itemid { get; set; }
+    }
+
+    public class ItemBlueprint
+    {
+        public List<ItemAmount> ingredients { get; set; } = new List<ItemAmount>();
+        public bool defaultBlueprint { get; set; }
+        public bool userCraftable { get; set; }
+        public bool isResearchable { get; set; }
+        public Rarity rarity { get; set; }
+        public int workbenchLevelRequired { get; set; }
+        public int scrapRequired { get; set; }
+        public int scrapFromRecycle { get; set; }
+        public bool NeedsSteamItem { get; set; }
+        public int blueprintStackSize { get; set; }
+        public float time { get; set; }
+        public int amountToCreate { get; set; }
+        public string UnlockAchievment { get; set; }
+    }
+
+    public class ObjectRef
+    {
+        public string guid { get; set; }
+    }
+
+    public class MCurve
+    {
+        public string serializedVersion { get; set; }
+        public double time { get; set; }
+        public double value { get; set; }
+        public double inSlope { get; set; }
+        public double outSlope { get; set; }
+        public int tangentMode { get; set; }
+        public int weightedMode { get; set; }
+        public double inWeight { get; set; }
+        public double outWeight { get; set; }
+    }
+
+    public class SpreadScalar
+    {
+        public string serializedVersion { get; set; }
+        public List<MCurve> m_Curve { get; set; }
+        public int m_PreInfinity { get; set; }
+        public int m_PostInfinity { get; set; }
+        public int m_RotationOrder { get; set; }
+    }
+
+    [Flags]
+    [JsonConverter(typeof(StringEnumConverter))]
+    public enum AmmoTypes
+    {
+        PISTOL_9MM = 1,
+        RIFLE_556MM = 2,
+        SHOTGUN_12GUAGE = 4,
+        BOW_ARROW = 8,
+        HANDMADE_SHELL = 0x10,
+        ROCKET = 0x20,
+        NAILS = 0x40
+    }
+
+    public class ItemMod
+    {
+
+    }
+
+    public class ItemModProjectileMod
+    {
+
+    }
+
+    public class ItemModProjectile : ItemMod
+    {
+        public ObjectRef projectileObject { get; set; }
+        public List<ItemModProjectileMod> mods { get; set; }
+        public AmmoTypes ammoType { get; set; }
+        public int numProjectiles { get; set; }
+        public double projectileSpread { get; set; }
+        public double projectileVelocity { get; set; }
+        public double projectileVelocitySpread { get; set; }
+        public bool useCurve { get; set; }
+        public SpreadScalar spreadScalar { get; set; }
+        public string category { get; set; }
+    }
+
+    public class ItemModBurnable : ItemMod
+    {
+        public float fuelAmount { get; set; }
+        public ItemDefinition byproductItem { get; set; }
+        public int byproductAmount { get; set; }
+        public float byproductChance { get; set; }
+    }
+
+    public class ItemModEntity : ItemMod
+    {
+        public ObjectRef EntityPrefab { get; set; }
+        public string DefaultBone { get; set; }
+    }
+}
---
+++ rust_wipe/Program.cs
@@ -0,0 +1,214 @@
+﻿using Newtonsoft.Json;
+using SilentOrbit.ProtocolBuffers;
+using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+
+namespace rust_wipe
+{
+    class Program
+    {
+        static Task Main(string[] args)
+        {
+            Console.WriteLine("v0l partial rust blueprint wiper v0.1\n");
+            return RunTasks();
+        }
+
+        private static async Task<List<ItemDefinition>> LoadItemDefs()
+        {
+            string url = "https://raw.githubusercontent.com/v0l/public/master/rust_items.json";
+
+            var json = await new WebClient().DownloadStringTaskAsync(new Uri(url));
+
+            return JsonConvert.DeserializeObject<List<ItemDefinition>>(json);
+        }
+
+        private static async Task RunTasks()
+        {
+            var cfg = JsonConvert.DeserializeObject<Config>(await File.ReadAllTextAsync("config.json"));
+
+            await WipeBlueprints(cfg);
+        }
+
+        private static async Task<Dictionary<string, Player>> GetSteamProfileInfo(Config cfg, IEnumerable<string> ids)
+        {
+            var rsp = JsonConvert.DeserializeObject<SteamApiResponse>(await new WebClient().DownloadStringTaskAsync($"https://api.steampowered.com/ISteamUser/GetPlayerSummaries/v2/?key={cfg.SteamApiKey}&steamids={string.Join(",", ids)}"));
+
+            return rsp.response.players.GroupBy(a => a.steamid).ToDictionary(a => a.Key, b => b.First());
+        }
+
+        private static async Task WipeBlueprints(Config cfg)
+        {
+            using (var con = new SQLiteConnection($"Data Source={cfg.DBPath}"))
+            {
+                await con.OpenAsync();
+
+                var items = await LoadItemDefs();
+                var item_dict = items.GroupBy(a => a.itemid).ToDictionary(a => a.Key, b => b.FirstOrDefault());
+
+                Console.WriteLine($"Wiping: \n\t{string.Join("\n\t", cfg.WipeList)}\n");
+
+                var wipe_items = item_dict.Where(a => cfg.WipeList.Contains(a.Value.shortname)).ToDictionary(a => a.Key, b => b.Value);
+
+                var bps = await GetBlueprints(con, item_dict);
+
+                //load user names if steamapikey is set
+                Dictionary<string, string> names = new Dictionary<string, string>();
+                if (!string.IsNullOrEmpty(cfg.SteamApiKey))
+                {
+                    var uids = bps.Keys.Distinct();
+                    while (uids.Count() > 0)
+                    {
+                        var batch = uids.Take(100);
+                        foreach (var profile in await GetSteamProfileInfo(cfg, batch))
+                        {
+                            names.Add(profile.Key, profile.Value.personaname);
+                        }
+                        uids = uids.Skip(100);
+                    }
+                }
+
+                Console.WriteLine($"steamid{(!string.IsNullOrEmpty(cfg.SteamApiKey) ? ",username" : "")},blueprints_known,blueprints_removed");
+                foreach (var userbp in bps.OrderByDescending(a => a.Value.Count))
+                {
+                    var removed = userbp.Value.RemoveWhere(a => wipe_items.ContainsKey(a));
+                    Console.WriteLine($"{(!string.IsNullOrEmpty(cfg.SteamApiKey) ? $"{userbp.Key},{names[userbp.Key]}" : userbp.Key)},{userbp.Value.Count},{removed}");
+                }
+
+                await UpdateBlueprints(con, bps);
+
+                con.Close();
+                Console.ReadKey();
+            }
+        }
+
+        private static async Task UpdateBlueprints(SQLiteConnection con, Dictionary<string, HashSet<int>> newIds)
+        {
+            using (var cmd = new SQLiteCommand("update data set info = @info where userid = @user", con))
+            {
+                foreach (var ubp in newIds)
+                {
+                    cmd.Parameters.Clear();
+                    cmd.Parameters.AddWithValue("info", SerializeItemList(ubp.Value));
+                    cmd.Parameters.AddWithValue("user", ubp.Key);
+                    await cmd.ExecuteNonQueryAsync();
+                }
+            }
+        }
+
+        private static byte[] SerializeItemList(HashSet<int> items)
+        {
+            using (var ms = new MemoryStream())
+            {
+                foreach (var item in items)
+                {
+                    ProtocolParser.WriteKey(ms, new Key(3, Wire.Varint));
+                    ProtocolParser.WriteUInt64(ms, (ulong)item);
+                }
+
+                // i have no idea why this is on the end... but ok
+                // field 100 wiretype variant with value 0
+                // yep this makes sense, should be until EOF??? 
+                ms.Write(new byte[]
+                {
+                    0xA0, 0x06, 0x00
+                });
+
+                return ms.ToArray();
+            }
+        }
+
+        private static async Task<Dictionary<string, HashSet<int>>> GetBlueprints(SQLiteConnection con, Dictionary<int, ItemDefinition> item_dict)
+        {
+            var ret = new Dictionary<string, HashSet<int>>();
+
+            using (var cmd_get = new SQLiteCommand("select * from data", con))
+            {
+                using (var r = await cmd_get.ExecuteReaderAsync())
+                {
+                    while (await r.ReadAsync())
+                    {
+                        var uid = r[0] as string;
+                        var bp_data = r[1] as byte[];
+                        if (!string.IsNullOrEmpty(uid) && bp_data != null)
+                        {
+                            using (var bp_ms = new MemoryStream(bp_data))
+                            {
+                                Key k;
+                                while ((k = ProtocolParser.ReadKey(bp_ms)).Field != 100)
+                                {
+                                    int itemid = 0;
+                                    if (k.WireType == Wire.Varint)
+                                    {
+                                        itemid = (int)ProtocolParser.ReadUInt64(bp_ms);
+                                    }
+
+                                    //add to user
+                                    if (itemid != 0 && item_dict.ContainsKey(itemid))
+                                    {
+                                        if (!ret.ContainsKey(uid))
+                                        {
+                                            ret.Add(uid, new HashSet<int>() { itemid });
+                                        }
+                                        else
+                                        {
+                                            if (!ret[uid].Contains(itemid))
+                                            {
+                                                ret[uid].Add(itemid);
+                                            }
+                                            else
+                                            {
+                                                Console.WriteLine($"{uid} had a duplicate item: {itemid}");
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            return ret;
+        }
+    }
+
+    internal class Config
+    {
+        public string DBPath { get; set; }
+        public string SteamApiKey { get; set; }
+        public string[] WipeList { get; set; }
+    }
+
+    internal class Player
+    {
+        public string steamid { get; set; }
+        public int communityvisibilitystate { get; set; }
+        public int profilestate { get; set; }
+        public string personaname { get; set; }
+        public int lastlogoff { get; set; }
+        public string profileurl { get; set; }
+        public string avatar { get; set; }
+        public string avatarmedium { get; set; }
+        public string avatarfull { get; set; }
+        public int personastate { get; set; }
+        public string primaryclanid { get; set; }
+        public int timecreated { get; set; }
+        public int personastateflags { get; set; }
+        public string loccountrycode { get; set; }
+    }
+
+    internal class Response
+    {
+        public List<Player> players { get; set; }
+    }
+
+    internal class SteamApiResponse
+    {
+        public Response response { get; set; }
+    }
+}
